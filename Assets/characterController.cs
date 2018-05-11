e97560df404cd154585326e7445762c836ca6fdd
Cormac Neeson <neesonch@tcd.ie>
2018-05-11 20:36:38 +0100

    feat: basic character WASD movement / Readme typo fix

---
+++ Assets/characterController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour {
+
+    public float speed = 5.0f;
+
+    // Use this for initialization
+    void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // CN 11/05/18: WASD movement
+        float forwardOrBack = Input.GetAxis("Vertical") * speed;
+        float leftOrRight = Input.GetAxis("Horizontal") * speed;
+        forwardOrBack = forwardOrBack * Time.deltaTime;
+        leftOrRight = leftOrRight * Time.deltaTime;
+        transform.Translate(leftOrRight, 0, forwardOrBack);
+		
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Cursor.lockState = CursorLockMode.None;
+        }
+	}
+}
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# ShadowOfTheKraken
+# Shadow Of The Kraken
 
-To set up Unity editor to play nice with GitHb, please follow the instructions [here](https://robots.thoughtbot.com/how-to-git-with-unity) - particularly Step 3 (installing Git LFS).
+To set up Unity editor to play nice with GitHub, please follow the instructions [here](https://robots.thoughtbot.com/how-to-git-with-unity) - particularly Step 3 (installing Git LFS).
